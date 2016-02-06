ffe624975e746e50b16b885c676655241f843b4f
TomiGie <paiguchi@gmail.com>
2016-02-06 17:35:42 +0900

    delete import

--- app/src/main/java/jp/itnav/freehandcropsample/FreeHandCropView.java
+++ app/src/main/java/jp/itnav/freehandcropsample/FreeHandCropView.java
@@ -15,7 +15,6 @@ import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
-import android.widget.ImageView;
 
 import java.util.ArrayList;
 import java.util.List;
